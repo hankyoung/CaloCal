0e814b694ac8fcd900d2980467924f75e7923b69
hankyoung <trannhatanhttqtd@gmail.com>
2020-11-01 16:39:09 +0700

    Boiler plate

---
+++ app/config/environment/Production.js
@@ -0,0 +1,6 @@
+import packageJson from '../../package.json';
+export default {
+  BASE_URL: 'abc.com',
+  API_KEY: '',
+  REACT_NATIVE_VERSION: packageJson.version,
+};
---
+++ app/config/index.js
@@ -0,0 +1,26 @@
+import Develop from '../config/environment/Develop';
+import Production from '../config/environment/Production';
+import Staging from '../config/environment/Staging';
+import IntegerResources from '../config/resources/integer';
+import StringResources from '../config/resources/string';
+
+const ENV = 'Develop'; // 'Develop'; 'Production'; 'Staging'
+
+export function geConfigs() {
+  switch (ENV) {
+    case 'Production':
+      return Production;
+    case 'Staging':
+      return Staging;
+    default:
+      return Develop;
+  }
+}
+
+export function getIntegerResources() {
+  return IntegerResources;
+}
+
+export function getStringResources() {
+  return StringResources;
+}
